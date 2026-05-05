f14e9997151b5056901af8f82b636bb9e7348123
Justin <justindeebwork@gmail.com>
2026-05-05 03:48:11 -0400

    added register

---
+++ client/src/components/Register.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import { registerUser } from "../utils/api";
+
+function Register(){
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const [message, setMessage] = useState('');
+    const {username, password} = formData;
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = async e =>{
+        e.preventDefault();
+        const res = await registerUser(username, password);
+        if (res.token){
+            localStorage.setItem('token', res.token);
+            localStorage.setItem('username', username);
+            localStorage.setItem('role', res.role);
+            window.location.href = '/';
+        }
+    };
+    return(
+        <div>
+            <div>
+                <form onSubmit = {onSubmit}>
+                    <input type = "text" placeholder = "Username" name = "username" value = {username} onChange = {onChange} required/>
+                    <input type = "password" placeholder = "Password" name = "password" value={password} onChange = {onChange} required />
+                    <button type = "submit">Register</button>
+                </form>
+                <p>{message}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
